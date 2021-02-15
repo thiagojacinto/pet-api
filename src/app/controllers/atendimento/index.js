3d207e91b3f0b63d8d34d6283171ecb462b1ff52
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2021-02-15 19:09:07 -0300

    feat: ajusting controller to handle interaction with service

--- src/app/controllers/atendimento/index.js
+++ src/app/controllers/atendimento/index.js
@@ -1,12 +1,18 @@
 const { Router } = require("express");
 const atendimentoRouter = Router();
+const Atendimento = require("../../services/atendimento");
 
 atendimentoRouter.get('/', function (req, res) {
   res.status(200).json({ atendimentoId: 1, info: 'Info' });
 });
 
-atendimentoRouter.post('/', function(req, res) {
-  res.status(201).json({ atendimentoId: 1 });
+atendimentoRouter.post('/', async function(req, res) {
+  const dbResponse = await Atendimento.registrar(req.body);
+  return res.status(201).send({
+    id: dbResponse.OkPacket.insertId,
+    client: req.body.client,
+    pet: req.body.pet,
+  });
 });
 
 module.exports = atendimentoRouter

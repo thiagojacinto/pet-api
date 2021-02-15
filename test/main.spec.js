1aded09546fe60fa1184087d91da31ec81f4bde2
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2021-02-15 19:09:38 -0300

    test: testing registering an Atendimento

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const server = require("../src/config")
+const server = require("../src/config");
 
 const serverInstance = server().app.listen();
 const test = request(serverInstance);
@@ -16,7 +16,32 @@ test
   .end(function (err, res) {
     if (err) throw err;
     console.log("Main :: Test passed.");
+    // serverInstance.close((err) => {
+    //   if (err) console.error(err);
+    // });
+  });
+
+/**
+ * GIVEN have a correct filled atendimento properties
+ * WHEN post it to /atendimentos
+ * THEN a new atendendimento must be created
+ */
+test
+  .post("/atendimento")
+  .send({
+    client: "Client n.1",
+    pet: "Abdiel",
+    service: "Cleaning",
+    status: "initialized",
+    isFinalized: false,
+  })
+  .expect(201)
+  .expect({ client: "Client n.1", pet: "Abdiel" })
+  .end(function (err, res) {
+    if (err) throw err;
+    console.log("Register atendimento :: Test passed.");
     serverInstance.close((err) => {
-      if (err) console.error(err)
+      if (err) console.error(err);
     });
   });
+  

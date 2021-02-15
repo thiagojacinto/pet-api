cb23b0ecf498dd52fc1e878e06fc0eca7a336411
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2021-02-15 15:17:02 -0300

    test: test now auto-closing

--- test/main.spec.js
+++ test/main.spec.js
@@ -1,10 +1,22 @@
 const request = require("supertest");
 const server = require("../src/config")
 
-request(server().app.listen())
+const serverInstance = server().app.listen();
+const test = request(serverInstance);
+
+/**
+ * WHEN access home "/"
+ * THEN should redirect to "/atendimento"
+ * AND status should be 301
+ */
+test
   .get("/")
-  .expect("Content-Type", /json/)
-  .expect(200)
+  .expect("Location", "/atendimento")
+  .expect(301)
   .end(function (err, res) {
     if (err) throw err;
+    console.log("Main :: Test passed.");
+    serverInstance.close((err) => {
+      if (err) console.error(err)
+    });
   });

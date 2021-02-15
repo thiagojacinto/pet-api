36c65da23a7dc4f6d19bfcb6bc515ca0fe57a720
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2021-02-15 20:50:04 -0300

    feat: updating db to handle datetime & service to handle that request

--- src/app/services/atendimento.js
+++ src/app/services/atendimento.js
@@ -1,10 +1,15 @@
+const moment = require("moment");
 const db = require("../../infra/db/mysql");
 
 class Atendimento {
   async registrar(atendimento)  {
+    atendimento.updatedAt = moment().format("YYYY-MM-DD HH:MM:SS");
+    const createdAt = moment(atendimento.createdAt, "DD-MM-YYYY").format(
+      "YYYY-MM-DD HH:MM:SS"
+    );
     const sql = "INSERT INTO atendimentos SET ?";
 
-    return await db.query(sql, atendimento, function(err, response) {
+    return await db.query(sql, {...atendimento, createdAt}, function(err, response) {
       if (err) console.error(err)
       else {
         console.log("Atendimento registered :: ", response);

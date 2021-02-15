5c4411c1c440978a39122d7f133241bb032dd2b5
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2021-02-15 14:03:36 -0300

    feat: using Router into app config

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const Logger = require("../infra/logger");
+const AtentimentoRouter = require("../app/controllers/atendimento");
 
 module.exports = () => {
   const PORT = 3001;
   const app = express();
 
   app.use(Logger);
+  app.use('/', express.Router().get('/', function(req, res) {
+    res.status(301).location('/atendimento')
+  }));
+  app.use('/atendimento', AtentimentoRouter);
 
   return {app, PORT};
 }

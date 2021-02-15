f4c87cb7d465ac81ed17db5fb5d865a3de2bc160
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2021-02-15 14:03:19 -0300

    feat: POST method to /atendimento

--- src/app/controllers/atendimento/index.js
+++ src/app/controllers/atendimento/index.js
@@ -1,1 +1,12 @@
-module.exports = app => app.get()
+const { Router } = require("express");
+const atendimentoRouter = Router();
+
+atendimentoRouter.get('/', function (req, res) {
+  res.status(200).json({ atendimentoId: 1, info: 'Info' });
+});
+
+atendimentoRouter.post('/', function(req, res) {
+  res.status(201).json({ atendimentoId: 1 });
+});
+
+module.exports = atendimentoRouter

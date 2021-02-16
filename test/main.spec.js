d850715c1498e9e48496e4387dc6552c78e9a9e0
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2021-02-15 23:16:13 -0300

    test: register atendimento input validation tests

--- test/main.spec.js
+++ test/main.spec.js
@@ -16,9 +16,6 @@ test
   .end(function (err, res) {
     if (err) throw err;
     console.log("Main :: Test passed.");
-    // serverInstance.close((err) => {
-    //   if (err) console.error(err);
-    // });
   });
 
 /**
@@ -34,11 +31,11 @@ test
     service: "Cleaning",
     status: "initialized",
     isFinalized: false,
-    createdAt: "17-01-2021 12:06:56",
+    createdAt: "17-02-2021 12:06:56",
   })
   .expect(201)
-  .expect(function(res) {
-    if (!('insertId' in res.body)) throw new Error('Missing insertId');
+  .expect(function (res) {
+    if (!("insertId" in res.body)) throw new Error("Missing insertId");
   })
   .end(function (err, res) {
     if (err) {
@@ -46,8 +43,67 @@ test
     } else {
       console.log("Register atendimento :: Test passed.");
     }
+    // serverInstance.close((err) => {
+    //   if (err) console.error(err);
+    // });
+  });
+
+/**
+ * GIVEN a small client name into atendimento
+ * WHEN post it to /atendimentos
+ * THEN name validator error should be thrown
+ */
+test
+  .post("/atendimento")
+  .send({
+    client: "Cl",
+    pet: "Snoopy",
+    service: "Washing",
+    status: "progress",
+    isFinalized: false,
+    createdAt: "17-03-2021 10:12:05",
+  })
+  .expect(400)
+  .expect(function (res) {
+    if (res.body[0].field !== "client") throw new Error("Missing client");
+  })
+  .end(function (err, res) {
+    if (err) {
+      console.log("Register atendimento :: Invalid name :: Test failed.", err);
+    } else {
+      console.log("Register atendimento :: Invalid name :: Test passed.");
+    }
+    // serverInstance.close((err) => {
+    //   if (err) console.error(err);
+    // });
+  });
+
+/**
+ * GIVEN a wrong date input in atendimento
+ * WHEN post it to /atendimentos
+ * THEN name validator error should be thrown
+ */
+test
+  .post("/atendimento")
+  .send({
+    client: "Oh-my-zsh",
+    pet: "Golden",
+    service: "Cut",
+    status: "progress",
+    isFinalized: false,
+    createdAt: "17-12-2018 15:11:05",
+  })
+  .expect(400)
+  .expect(function (res) {
+    if (res.body[0].field !== "createdAt") throw new Error("Missing createdAt");
+  })
+  .end(function (err, res) {
+    if (err) {
+      console.log("Register atendimento :: Invalid createdAt :: Test failed.", err);
+    } else {
+      console.log("Register atendimento :: Invalid createdAt :: Test passed.");
+    }
     serverInstance.close((err) => {
       if (err) console.error(err);
     });
   });
-  

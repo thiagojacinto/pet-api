c2ff8b042d6cb9d14738a73e2dff2b344aaf577d
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2021-02-15 23:17:39 -0300

    feat: atendimento register input validators

---
+++ src/app/domain/validators/atendimento.js
@@ -0,0 +1,27 @@
+const moment = require("moment");
+const atendimento = require("../../services/atendimento");
+
+const isDateValid = (date, updatedAt) => {
+  return moment(date).isSameOrAfter(updatedAt);
+};
+
+const isNameValid = (name) => {
+  return name.length >= 3;
+}
+
+const validations = (atendimento) => ([
+  {
+    field: 'createdAt',
+    isValid: isDateValid(atendimento.createdAt, atendimento.updatedAt),
+    message: 'Date must be equal or later than now.'
+  },
+  {
+    field: 'client',
+    isValid: isNameValid(atendimento.client),
+    message: 'Name must be at least 3 characters long.'
+  }
+]);
+
+const errors = (atendimento) => (validations(atendimento).filter(item => !item.isValid));
+
+module.exports = errors;
--- src/app/services/atendimento.js
+++ src/app/services/atendimento.js
@@ -1,20 +1,28 @@
 const moment = require("moment");
 const db = require("../../infra/db/mysql");
+const errors = require("../domain/validators/atendimento");
 
 class Atendimento {
   registrar(atendimento, res)  {
     const updatedAt = moment().format("YYYY-MM-DD hh:mm:ss");
     const createdAt = moment(atendimento.createdAt, "DD-MM-YYYY hh:mm:ss").format("YYYY-MM-DD hh:mm:ss");
     const postData = { ...atendimento, createdAt, updatedAt };
-    const sql = "INSERT INTO atendimentos SET ?";
 
-    db.query(sql, postData, (err, result) => {
-      if (err) {
-        res.status(400).json(err);
-      } else {
-        res.status(201).json(result);
-      }
-    });
+    const validationErrors = errors(postData);
+    
+    if (validationErrors.length) {
+      res.status(400).json(validationErrors);
+    } else {
+      const sql = "INSERT INTO atendimentos SET ?";
+      
+      db.query(sql, postData, (err, result) => {
+        if (err) {
+          res.status(400).json(err);
+        } else {
+          res.status(201).json(result);
+        }
+      });
+    };
   };
 };
 

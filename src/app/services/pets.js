24275a6379a33c5bb56846673bfad86051c48d49
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2021-02-16 14:11:12 -0300

    feat: service to handle image uploading when register a new Pet

--- src/app/services/pets.js
+++ src/app/services/pets.js
@@ -1,4 +1,6 @@
 const db = require("../../infra/db/mysql");
+const uploadStreamImage = require("../../utils/uploadImage.utils");
+const petValidator = require("../domain/validators/imageExtensions");
 
 class Pet {
 
@@ -8,20 +10,32 @@ class Pet {
    * @param {*} res Express.Response
    */
   add(pet, res) {
-    const sql = "INSERT INTO pets SET ?";
+    const validatorErrors = petValidator(pet);
 
-    db.query(sql, pet, (err, results) => {
-      if (err) {
-        res.status(400).json(err);
-      } else {
-        const { insertId } = results;
-        res.status(201).json({
-          id: insertId,
-          ...pet
-        })
-      }
-    })
+    if (validatorErrors.length) {
+      res.status(400).json(validatorErrors);
+    } else {
+      uploadStreamImage(pet.imageUri, (err, output) => {
+        if (err) {
+          res.status(400).json({ error: err });
+        } else {
+          const validatedPet = { imageUri: output, pet: pet.pet };
+          const sql = "INSERT INTO pets SET ?";
+           db.query(sql, validatedPet, (err, results) => {
+            if (err) {
+              res.status(400).json(err);
+            } else {
+              const { insertId } = results;
+              res.status(201).json({
+                id: insertId,
+                ...validatedPet
+              });
+            }
+          });
+        }
+      });
+    }
   }
 };
-
+    
 module.exports = new Pet();

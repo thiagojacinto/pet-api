249026f50c494c65abf01a0bb0e14960eb27132f
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2021-02-18 23:26:25 -0300

    feat: clarify use of promisified Client.getInfo()

--- src/app/controllers/atendimento/index.js
+++ src/app/controllers/atendimento/index.js
@@ -1,40 +1,35 @@
 const { Router } = require("express");
 const Atendimento = require("../../services/atendimento");
-const Client = require("../../services/client"); 
+const Client = require("../../services/client");
 
 const atendimentoRouter = Router();
 
-atendimentoRouter.patch('/:id', function (req, res) {
+atendimentoRouter.patch("/:id", function (req, res) {
   const { id } = req.params;
   Atendimento.atualizar(id, req.body, res);
 });
 
-atendimentoRouter.get('/', function (req, res) {
+atendimentoRouter.get("/", function (req, res) {
   Atendimento.listar()
-    .then(results => res.json(results))
-    .catch(err => res.status(400).json(err));
+    .then((results) => res.json(results))
+    .catch((err) => res.status(400).json(err));
 });
 
-atendimentoRouter.get('/:id', function (req, res) {
+atendimentoRouter.get("/:id", function (req, res) {
   const { id } = req.params;
   Atendimento.listarPorId(id, res);
 });
 
-atendimentoRouter.post('/', async function(req, res) {
+atendimentoRouter.post("/", async function (req, res) {
   const novoAtendimento = req.body;
   Atendimento.registrar(novoAtendimento)
-    .then(result => { 
-      Client.getInfo(novoAtendimento.client, (statusCode, resultClient) => {
-        if (statusCode == 200) {
-          novoAtendimento.client = resultClient;
-          res.status(201).json({
-            id: result.insertId,
-            ...novoAtendimento
-          });
-        }
+  .then(async (result) => {
+      novoAtendimento.client = await Client.getInfo(novoAtendimento.client);
+      res.status(201).json({
+        id: result.insertId,
+        ...novoAtendimento,
       });
-    })
-    .catch(err => res.status(400).json(err));
+    }).catch((err) => res.status(400).json(err));
 });
 
-module.exports = atendimentoRouter
+module.exports = atendimentoRouter;

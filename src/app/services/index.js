edc104112a82cc265a78675242a2872efca24d94
Thiago Jacinto <46906069+thiagojacinto@users.noreply.github.com>
2021-02-20 10:03:20 -0300

    feat: barrel for services

---
+++ src/app/services/index.js
@@ -0,0 +1,9 @@
+const AtendimentoService = require("./atendimento");
+const PetService = require("./pets");
+const ClientService = require("./client");
+
+module.exports = {
+  AtendimentoService,
+  PetService,
+  ClientService
+}
